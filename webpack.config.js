fbf8586f8aa0c3ca0bb94f846bb2582894bf9527
João Bispo <joaobispo2077@gmail.com>
2021-08-24 12:25:54 -0300

    feat: add fast refresh (without change state)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -19,13 +20,21 @@ module.exports = {
   },
   devServer: {
     static: path.resolve(__dirname, 'public'),
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel'),
+            ].filter(Boolean)
+          }
+        }
       },
       {
         test: /\.scss$/,
@@ -37,6 +46,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
-    })
-  ]
+    }),
+    isDevelopment && new ReactRefreshWebpackPlugin()
+  ].filter(Boolean)
 }
